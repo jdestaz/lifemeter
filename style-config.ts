1859bb8f6293acb06c24f02244661b464074c2a9
Joe DeStazio <joe.destazio@maritzcx.com>
2016-05-14 19:28:23 -0400

    Config

---
+++ style-config.ts
@@ -0,0 +1,30 @@
+﻿class StyleConfigs {
+	styles: StyleConfig[] = [];
+
+	constructor() {
+		this.styles.push({
+			name: "Mega Man (8-bit)",
+			shortName: "megaman",
+			className: "MegaManStyle",
+			width: 300,
+			height: 300
+		});
+
+		this.styles.push({
+			name: "Shovel Knight",
+			shortName: "shovelknight",
+			className: "ShovelKnightStyle",
+			width: 300,
+			height: 300
+		});
+	}
+}
+
+
+interface StyleConfig {
+	name: string;
+	shortName: string;
+	className: string;
+	width: number;
+	height: number;
+}

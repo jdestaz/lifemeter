7186e061b0da606ba6c922457ca8a375e3d783d8
Joe DeStazio <joe.destazio@maritzcx.com>
2016-11-08 16:24:57 -0500

    Added Kirby's Adventure style

---
+++ styles/kirbyadventure/src.js
@@ -0,0 +1,56 @@
+var styles = styles || [];
+
+styles.push({
+    name: "Kirby's Adventure",
+    shortName: "kirbyadventure",
+    width: 300,
+    height: 300,
+    
+    BARWIDTH: 14,
+	BARPADDING: 2,
+	MAXBARS: 6,
+	YPOS: 214,
+	STARTXPOS: 160,
+
+    renderTo(canvasWriter, styleService, calculatorResults) {
+
+        var imageSources = {
+            bg: styleService.getImagePath('bg.png', this),
+			fullBar: styleService.getImagePath('fullbar.png', this),
+			emptyBar: styleService.getImagePath('emptybar.png', this)
+        };
+
+		canvasWriter.loadImages(imageSources, images => {
+			canvasWriter.drawImageToCanvas(images.bg, 0, 0);
+
+			var currentHealth = calculatorResults.meterValue * this.MAXBARS;
+
+			if (currentHealth < .5) {
+				currentHealth = .5;
+			}
+
+			var numBars = Math.round(currentHealth);
+
+			var currentPos = 0;
+
+			for (var i = 0; i < numBars; i++) {
+				this.drawBar(canvasWriter, images.fullBar, currentPos);
+				currentPos++;
+			}
+
+			for (var i = currentPos; i < this.MAXBARS; i++) {
+				this.drawBar(canvasWriter, images.emptyBar, currentPos);
+				currentPos++;
+			}
+		});
+	},
+
+    getCurrentXPosition(currentPos) {
+		return this.STARTXPOS + (currentPos * (this.BARWIDTH + this.BARPADDING));
+	},
+
+	drawBar(canvasWriter, image, currentPos) {
+		canvasWriter.drawImageToCanvas(image, this.getCurrentXPosition(currentPos), this.YPOS);
+	}
+
+});
